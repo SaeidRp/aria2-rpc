8644ab0327b1cab5797cbf3468f3b3f6cb53f55f
SaeidRp <saeed.rahmanpour@gmail.com>
2024-05-21 15:27:55 +0330

    feat(resp): Add `GetDownloadSpeed` and `GetUploadSpeed` methods

--- resp.go
+++ resp.go
@@ -49,6 +49,14 @@ func (s *StatusInfo) GetCompletedLength() int64 {
 	return StrToInt64(s.CompletedLength)
 }
 
+func (s *StatusInfo) GetDownloadSpeed() int64 {
+	return StrToInt64(s.DownloadSpeed)
+}
+
+func (s *StatusInfo) GetUploadSpeed() int64 {
+	return StrToInt64(s.UploadSpeed)
+}
+
 func StrToInt64(s string) int64 {
 	i, _ := strconv.ParseInt(s, 10, 64)
 	return i
